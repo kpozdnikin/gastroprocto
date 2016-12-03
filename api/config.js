4d33d68ab0aa53cb4fbabdfd604617c9a0cfdf28
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2016-12-03 15:34:19 +0300

    api 1st

---
+++ api/config.js
@@ -0,0 +1,5 @@
+var configs = {};
+configs.applicationPort = 3000;
+configs.dbName = 'Gastroprocto articles blog';
+configs.dbHost = 'localhost';
+module.exports = configs;
---
+++ api/routes/api.js
@@ -0,0 +1,17 @@
+//var users = require('../controllers/users');
+var articles = require('../controllers/articles');
+//var helpers = require('../helpers/helperFunctions');
+
+var routesAPI = function(app){
+	//user routes
+	//app.post('/user/auth', users.auth);
+	//app.get('/user/logout', helpers.isAuthenticated, users.logout);
+
+	//articles routes
+	//app.get('/articles', helpers.isAuthenticated, articles.get);
+	//app.get('/article', helpers.isAuthenticated, articles.getOne);
+	//app.post('/articles/comments', helpers.isAuthenticated, videos.rate);
+}
+
+
+module.exports = routesAPI;
